a6ffa5ed8cd9ac0d42d34a67350ec30168af3349
surViVeCZ <poucpetr11@seznam.cz>
2023-03-16 18:33:22 +0100

    výpis čísel

--- makefile
+++ makefile
@@ -3,7 +3,7 @@ CXXFLAGS = -Wall -std=c++11
 
 all: parsplit
 
-parsplit: parsplit.c
+parsplit: parsplit.cc
 	$(CXX) $(CXXFLAGS) --prefix /usr/local/share/OpenMPI -o $@ $<
 
 clean:
---
+++ parsplit.cc
@@ -0,0 +1,54 @@
+#include <mpi.h>
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <iostream>
+#include <vector>   
+using namespace std;
+//open file numbers and read it
+
+
+//function calcultes number of bytes of binary file
+int file_size(ifstream& file){
+    file.seekg(0, ios::end);
+    int length = file.tellg();
+    file.seekg(0, ios::beg);
+    return length;
+}
+
+int main(int argc, char** argv) {
+    // Initialize the MPI environment
+    MPI_Init(&argc, &argv);
+
+    int process_no;
+    MPI_Comm_rank(MPI_COMM_WORLD, &process_no);
+    //opening binary file
+    if (process_no == 0) {
+        ifstream input("numbers", ios::binary);
+    
+        // Check if the file was opened successfully
+        if (!input){
+            cerr << "Failed to open file" << endl;
+            return 1;
+        }
+
+        
+        int length = file_size(input);
+        char buffer[64];
+        input.read(buffer, 64);
+
+        input.close();
+
+        // Output the numbers to the console
+        for (int i = 0; i < length; i++){
+            cout << static_cast<int>(buffer[i]) << " ";
+        }
+        cout << endl;
+    }
+ 
+
+    std::cout << "Hello from process " << process_no << "!" << std::endl;
+
+    MPI_Finalize();
+    return 0;
+}
