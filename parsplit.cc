caf5030e0ad7aae336d82f0c02b8fa41b766f2cf
surViVeCZ <poucpetr11@seznam.cz>
2023-03-21 14:28:09 +0100

    scatter works

--- parsplit.cc
+++ parsplit.cc
@@ -16,7 +16,6 @@ int file_size(ifstream& file){
 }
 
 int main(int argc, char** argv) {
-    // Initialize the MPI environment
     MPI_Init(&argc, &argv);
 
     int process_no, N; //number of processes
@@ -36,61 +35,60 @@ int main(int argc, char** argv) {
     int n = file_size(input);
     int n_per_process = n / N;
 
-  
+    // Determine the starting position and length of the data for this process
+    int start_pos = process_no * n_per_process;
+    int end_pos = (process_no == N - 1) ? n : (start_pos + n_per_process);
+    int len = end_pos - start_pos;
 
-    // Compute the median on the root process
-    int median;
-     //opening binary file
+    // Allocate memory for local data
+    std::vector<unsigned char> local_numbers(len);
+
     if (process_no == 0) {
-        std::ifstream input("numbers", std::ios::binary);
-    
-        // Check if the file was opened successfully
-        if (!input){
-            std::cerr << "Failed to open file" << std::endl;
-            return 1;
-        }
-
-        int n = file_size(input);
+        // Read in the data for the root process from the binary file
         std::vector<unsigned char> numbers_file(n);
         input.read(reinterpret_cast<char*>(&numbers_file[0]), n);
-
         input.close();
+
         // Output the numbers to the console
         for (int i = 0; i < n; i++){
             std::cout << static_cast<int>(numbers_file[i]) << " ";
         }
         std::cout << std::endl;
 
-        median = numbers_file[n/2];
+        // Scatter the data to all processes
+        MPI_Scatter(&numbers_file[0], len, MPI_UNSIGNED_CHAR,
+                    &local_numbers[0], len, MPI_UNSIGNED_CHAR,
+                    0, MPI_COMM_WORLD);
+    } else {
+        // Allocate memory for root process data
+        std::vector<unsigned char> numbers_file(n);
+
+        // Scatter the data to all processes
+        MPI_Scatter(NULL, len, MPI_UNSIGNED_CHAR,
+                    &local_numbers[0], len, MPI_UNSIGNED_CHAR,
+                    0, MPI_COMM_WORLD);
+    }
+
+    // Compute the median on the root process
+    int median;
+    if (process_no == 0) {
+        median = local_numbers[n_per_process/2];
         std::cout << "Median: " << median << std::endl;
     }
-      // Determine the starting position and length of the data for this process
-    int start_pos = process_no * n_per_process;
-    int end_pos = (process_no == N - 1) ? n : (start_pos + n_per_process);
-    int len = end_pos - start_pos;
-
-    // Read in the data for this process from the binary file
-    std::vector<unsigned char> numbers_file(len);
-    input.seekg(start_pos);
-    input.read(reinterpret_cast<char*>(&numbers_file[0]), len);
-    input.close();
-
-    //TODO rewrite using scatter
-    //split numbers into same size groups and send 1 group to each process using MPI_Scatter (first 2 numbers to process 0, next 2 to process 1, etc.)
-    // MPI_Scatter(&numbers_file[0], len, MPI_INT, &numbers_file[0], len, MPI_INT, 0, MPI_COMM_WORLD);
 
     // Broadcast the median to all processes
     MPI_Bcast(&median, 1, MPI_INT, 0, MPI_COMM_WORLD);
 
+
     // Compute the L, E, and G groups for this process
     std::vector<unsigned char> L, E, G;
     for (int i = 0; i < len; i++) {
-        if (numbers_file[i] < median) {
-            L.push_back(numbers_file[i]);
-        } else if (numbers_file[i] > median) {
-            G.push_back(numbers_file[i]);
+        if (local_numbers[i] < median) {
+            L.push_back(local_numbers[i]);
+        } else if (local_numbers[i] == median) {
+            E.push_back(local_numbers[i]);
         } else {
-            E.push_back(numbers_file[i]);
+            G.push_back(local_numbers[i]);
         }
     }
 
@@ -113,27 +111,53 @@ int main(int argc, char** argv) {
         std::cout << static_cast<int>(num) << " ";
     }
     std::cout << std::endl;
-
     
-    //compute sums of L, E, and G groups
-    int sum_L = 0, sum_E = 0, sum_G = 0;
-    for (auto num : L) {
-        sum_L += num;
-    }
-    for (auto num : E) {
-        sum_E += num;
-    }
-    for (auto num : G) {
-        sum_G += num;
-    }
 
-    //merge sum of L of each process
-    int sum_L_all = 0;
-    int sum_E_all = 0;
-    int sum_G_all = 0;
-    MPI_Reduce(&sum_L, &sum_L_all, 1, MPI_INT, MPI_SUM, 0, MPI_COMM_WORLD);
-    MPI_Reduce(&sum_E, &sum_E_all, 1, MPI_INT, MPI_SUM, 0, MPI_COMM_WORLD);
-    MPI_Reduce(&sum_G, &sum_G_all, 1, MPI_INT, MPI_SUM, 0, MPI_COMM_WORLD);
+    // Gather the L, E, and G groups from all processes
+    std::vector<unsigned char> L_all, E_all, G_all;
+    L_all.resize(N);
+    E_all.resize(N);
+    G_all.resize(N);
+
+    // MPI_Gather(&L[0], L.size(), MPI_UNSIGNED_CHAR,
+    //            &L_all[0], L.size(), MPI_UNSIGNED_CHAR,
+    //            0, MPI_COMM_WORLD);
+    
+    // MPI_Gather(&E[0], E.size(), MPI_UNSIGNED_CHAR,
+    //             &E_all[0], E.size(), MPI_UNSIGNED_CHAR,
+    //             0, MPI_COMM_WORLD);
+    
+    // MPI_Gather(&G[0], G.size(), MPI_UNSIGNED_CHAR,
+    //             &G_all[0], G.size(), MPI_UNSIGNED_CHAR,
+    //             0, MPI_COMM_WORLD);
+
+
+    // // Merge the L, E, and G groups into L_all, E_all, and G_all on the root process
+    // if (process_no == 0) {
+    //     // Determine the total size of L, E, and G across all processes
+    //     int total_L_size, total_E_size, total_G_size;
+    //     MPI_Reduce(&L.size(), &total_L_size, 1, MPI_INT, MPI_SUM, 0, MPI_COMM_WORLD);
+    //     MPI_Reduce(&E.size(), &total_E_size, 1, MPI_INT, MPI_SUM, 0, MPI_COMM_WORLD);
+    //     MPI_Reduce(&G.size(), &total_G_size, 1, MPI_INT, MPI_SUM, 0, MPI_COMM_WORLD);
+
+    //     // Resize L_all, E_all, and G_all to the total size of L, E, and G
+    //     L_all.resize(total_L_size);
+    //     E_all.resize(total_E_size);
+    //     G_all.resize(total_G_size);
+
+    //     // Gather the L, E, and G groups from all processes again, this time into L_all, E_all, and G_all
+    //     MPI_Gather(&L[0], L.size(), MPI_UNSIGNED_CHAR,
+    //             &L_all[0], L.size(), MPI_UNSIGNED_CHAR,
+    //             0, MPI_COMM_WORLD);
+    //     MPI_Gather(&E[0], E.size(), MPI_UNSIGNED_CHAR,
+    //             &E_all[0], E.size(), MPI_UNSIGNED_CHAR,
+    //             0, MPI_COMM_WORLD);
+    //     MPI_Gather(&G[0], G.size(), MPI_UNSIGNED_CHAR,
+    //             &G_all[0], G.size(), MPI_UNSIGNED_CHAR,
+    //             0, MPI_COMM_WORLD);
+    // }
+
+
 
 
     MPI_Finalize();
