cc5f88cc81d431daf81c8bd8c9ca5e5f9ab8b399
surViVeCZ <poucpetr11@seznam.cz>
2023-04-12 11:37:50 +0200

    find nearest cluster

--- proj2/parkmeans.cpp
+++ proj2/parkmeans.cpp
@@ -15,6 +15,23 @@ using namespace std;
 const int MAX_ITER = 100;
 const int n_means = 4;
 
+double distance(unsigned char value, double mean) {
+    return abs(value - mean);
+}
+
+int find_nearest_cluster(unsigned char value, const vector<double>& means) {
+    int cluster_index = -1;
+    double min_distance = INFINITY;
+    for (int i = 0; i < n_means; i++) {
+        double d = distance(value, means[i]);
+        if (d < min_distance) {
+            min_distance = d;
+            cluster_index = i;
+        }
+    }
+    return cluster_index;
+}
+
 int file_size(ifstream& file){
     file.seekg(0, ios::end);
     int n = file.tellg();
@@ -45,55 +62,14 @@ int find_nearest_cluster(double *value, double **clusters, int n, int m)
     return cluster_index;
 }
 
-void compute_clusters(double **values, int n, int m, double **clusters, int *assignments)
-{
-    for (int i = 0; i < n_means; i++) {
-        int idx = rand() % n;
-        for (int j = 0; j < m; j++) {
-            clusters[i][j] = values[idx][j];
-        }
-    }
-
-    bool changed = true;
-    int iter = 0;
-    while (changed && iter < MAX_ITER) {
-        changed = false;
-        for (int i = 0; i < n; i++) {
-            int nearest_cluster = find_nearest_cluster(values[i], clusters, n_means, m);
-            if (nearest_cluster != assignments[i]) {
-                changed = true;
-                assignments[i] = nearest_cluster;
-            }
-        }
-
-        for (int i = 0; i < n_means; i++) {
-            double *sum = new double[m];
-            int count = 0;
-            for (int j = 0; j < n; j++) {
-                if (assignments[j] == i) {
-                    count++;
-                    for (int k = 0; k < m; k++) {
-                        sum[k] += values[j][k];
-                    }
-                }
-            }
-            if (count > 0) {
-                for (int k = 0; k < m; k++) {
-                    clusters[i][k] = sum[k] / count;
-                }
-            }
-            delete[] sum;
-        }
-
-        iter++;
-    }
-}
 
 int main(int argc, char *argv[])
 {
 
     int process_no;
     int nprocs;
+    //create list of means of size n_means
+    std::vector<double> means(n_means);
     MPI_Init(&argc, &argv);
     MPI_Comm_rank(MPI_COMM_WORLD, &process_no);
     MPI_Comm_size(MPI_COMM_WORLD, &nprocs);
@@ -127,9 +103,9 @@ int main(int argc, char *argv[])
     }
 
     //read input numbers from binary file
+    std::vector<unsigned char> numbers_file(n);
     if (process_no == 0) {
         // Read in the data for the root process from the binary file
-        std::vector<unsigned char> numbers_file(n);
         input.read(reinterpret_cast<char*>(&numbers_file[0]), n);
         input.close();
 
@@ -147,10 +123,6 @@ int main(int argc, char *argv[])
             clusters[i % n_means].push_back(numbers_file[i]);
         }
      
-        //create list of means of size n_means
-        std::vector<double> means(n_means);
-    
-
         // Print the clusters to the console
         for (int i = 0; i < n_means; i++) {
             //print cluster mean from means list
@@ -175,56 +147,16 @@ int main(int argc, char *argv[])
         }
         
     }
+    // assign each number to its nearest cluster
+    std::vector<int> assignments(n);
+    for (int i = 0; i < n; i++) {
+        double value = numbers_file[i];
+        int cluster_index = find_nearest_cluster(value, means);
+        assignments[i] = cluster_index;
+    }
 
     int m = 1;   // number of dimensions
 
-    // Allocate memory for the values and assignments
-    double **values = new double *[n];
-    int *assignments = new int[n];
-
-    for (int i = 0; i < n; i++) {
-        values[i] = new double[m];
-        assignments[i] = -1;
-    }
-
-    // Generate random values
-    srand(12345);
-    for (int i = 0; i < n; i++) {
-        for (int j = 0; j < m; j++) {
-            values[i][j] = (double) rand() / RAND_MAX;
-        }
-    }
-
-    // Allocate memory for the clusters
-    double **clusters = new double *[n_means];
-    for (int i = 0; i < n_means; i++) {
-        clusters[i] = new double[m];
-    }
-
-    // Compute the clusters using K-Means
-    compute_clusters(values, n, m, clusters, assignments);
-
-    // Print the results
-    if (process_no == 0) {
-        printf("Clusters:\n");
-        for (int i = 0; i < n_means; i++) {
-            for (int j = 0; j < m; j++) {
-                printf("%f ", clusters[i][j]);
-            }
-            printf("\n");
-        }
-    }
-
-    // Deallocate memory
-    for (int i = 0; i < n; i++) {
-        delete[] values[i];
-    }
-    delete[] values;
-    delete[] assignments;
-    for (int i = 0; i < n_means; i++) {
-        delete[] clusters[i];
-    }
-    delete[] clusters;
 
     // Finalize MPI
     MPI_Finalize();
