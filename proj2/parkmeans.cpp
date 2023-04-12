a0e1d4133be66e7f813708828c1fe7af30b643cf
surViVeCZ <poucpetr11@seznam.cz>
2023-04-12 18:12:39 +0200

    mezikrok, kam se možná vrátím

--- proj2/parkmeans.cpp
+++ proj2/parkmeans.cpp
@@ -72,12 +72,15 @@ std::vector<int> new_clusters(int process_no, int n, const vector<unsigned char>
     //calculate new means
     std::vector<double> means(n_means);
     for (int i = 0; i < n_means; i++) {
-        double mean = 0;
+        double sum = 0;
         for (int j = 0; j < clusters[i].size(); j++) {
-            mean += clusters[i][j];
+            sum += clusters[i][j];
         }
-        mean /= clusters[i].size();
-        means[i] = mean;
+        if (clusters[i].size() > 0) {
+            means[i] = sum / clusters[i].size();
+        } else {
+            means[i] = 0;
+        }
     }
     // print clusters
     if (process_no == 0) {
@@ -101,8 +104,7 @@ std::vector<int> new_clusters(int process_no, int n, const vector<unsigned char>
 }
 
 
-int main(int argc, char *argv[])
-{
+int main(int argc, char* argv[]) {
     int process_no;
     int nprocs;
     //create list of means of size n_means
@@ -117,70 +119,56 @@ int main(int argc, char *argv[])
     int n = 0;
     if (process_no == 0) {
         n = file_size(input);
+
+        if (nprocs > n) {
+            std::cerr << "Too many processes" << std::endl;
+            MPI_Finalize();
+            return 1;
+        }
+    }
+  
+    if (n > nprocs) {
+        input.seekg(n-nprocs, ios::beg);
+        n = nprocs;
     }
     MPI_Bcast(&n, 1, MPI_INT, 0, MPI_COMM_WORLD);
-
-    const int chunk = n / nprocs;
-    std::vector<unsigned char> chunk_numbers_file(chunk);
-    MPI_Scatter(NULL, 0, MPI_DATATYPE_NULL, &chunk_numbers_file[0], chunk, MPI_UNSIGNED_CHAR, 0, MPI_COMM_WORLD);
-    //local mean for each process
-    std::vector<double> local_means(n_means);
 
     //read input numbers from binary file
     std::vector<unsigned char> numbers_file(n);
     std::vector<double> all_means(n_means * nprocs);
-    MPI_Gather(&means[0], n_means, MPI_DOUBLE, &all_means[0], n_means, MPI_DOUBLE, 0, MPI_COMM_WORLD);
 
     if (process_no == 0) {
-        // Read in the data for the root process from the binary file
+        // Read in the data for the root process from the binary file as integers
         input.read(reinterpret_cast<char*>(&numbers_file[0]), n);
         input.close();
 
-         // Split numbers_file into 4 clusters
-        std::vector<std::vector<unsigned char>> clusters(n_means);
-        for (int i = 0; i < n; i++) {
-            clusters[i % n_means].push_back(numbers_file[i]);
-        }
-     
+        //1.)Get cluster centers as 4 first numbers from file
+        printf("Initialized centers: ");
         for (int i = 0; i < n_means; i++) {
-            double mean = 0;
-            for (int j = 0; j < clusters[i].size(); j++) {
-                mean += clusters[i][j];
-            }
-            mean /= clusters[i].size();
-            means[i] = mean;
-
-            std::cout << "Cluster " << i+1 << ": ";
-            std::cout << std::setprecision(1) << std::fixed << "[" << means[i] << "]" << " ";
-            for (int j = 0; j < clusters[i].size(); j++) {
-                //for last dont print comma
-                if( j == clusters[i].size()-1)
-                    std::cout << static_cast<int>(clusters[i][j]);
-                else
-                    std::cout << static_cast<int>(clusters[i][j]) << ", ";
-            }
-            std::cout << std::endl;
+            means[i] = numbers_file[i];
+            printf("%d ", int(means[i]));
         }
-        //broadcast means to other processes
-        MPI_Bcast(&means[0], n_means, MPI_DOUBLE, 0, MPI_COMM_WORLD);
-    }
-    // assign each number to its nearest cluster
-    std::vector<int> assignments(n);
-    for (int i = 0; i < n; i++) {
-        int cluster_index = find_nearest_cluster(numbers_file[i], means);
-        assignments[i] = cluster_index;
-    }
-
-    if (process_no == 0) {
         std::cout << std::endl;
     }
-    //call new_clusters until assignments dont change
+    //send 1 number to each process using MPI scatter
+    MPI_Scatter(&numbers_file[0], 1, MPI_UNSIGNED_CHAR, &numbers_file[0], 1, MPI_UNSIGNED_CHAR, 0, MPI_COMM_WORLD);
+    //send means to all processes
+    MPI_Bcast(&means[0], n_means, MPI_DOUBLE, 0, MPI_COMM_WORLD);
+
+    //parallel implementation of 4-means algorithm, use MPI allgather and reduce to communicate
+    //2.)Assign each number to the nearest cluster center
+    std::vector<int> assignments(n);
+    for (int i = 0; i < n; i++) {
+        assignments[i] = find_nearest_cluster(numbers_file[i], means);
+    }
+    //3.)Calculate new cluster centers
+    //4.)Repeat steps 2 and 3 until convergence
     std::vector<int> new_assignments = new_clusters(process_no, n, numbers_file, assignments);
-    while (assignments != new_assignments) {
+    while(new_assignments != assignments) {
         assignments = new_assignments;
         new_assignments = new_clusters(process_no, n, numbers_file, assignments);
     }
-    new_assignments = new_clusters(process_no, n, numbers_file, assignments);
+
     MPI_Finalize();
     return 0;
 }
